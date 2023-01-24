719c64985aabcc1ae1aa5c092203adb8dad18d53
KGS97 <45433869+KGS97@users.noreply.github.com>
2023-01-25 01:42:55 +0530

    completed navbar

--- src/pages/Components/Navbar.jsx
+++ src/pages/Components/Navbar.jsx
@@ -3,12 +3,12 @@ import { BsMoonStarsFill } from "react-icons/bs";
 const Navbar = () => {
   let theme = "light";
   return (
-    <div className="flex flex-row items-center justify-between">
-      <h1 className="font-burtons text-xl">Dipanshu Singh</h1>
+    <div className="flex flex-row items-center justify-between pb-40">
+      <h1 className="font-burtons text-xl">Portfolio</h1>
       <div className="flex flex-row items-center gap-10 md:gap-15">
         <BsMoonStarsFill
           style={{ cursor: "pointer" }}
-          size={"30px"}
+          size={"28px"}
           onClick={() => {
             if (theme == "dark") {
               theme = "light";
@@ -20,7 +20,7 @@ const Navbar = () => {
           }}
         />
         <a href="">
-          <button className="text-white bg-gradient-to-r from-cyan-500 to-teal-500 rounded px-4 py-2">
+          <button className="text-white bg-gradient-to-r from-cyan-500 to-teal-500 rounded-md py-1.5 px-5">
             Resume
           </button>
         </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       fontFamily: {
         burtons: ["burtons"],
+        poppins: ["poppins"],
       },
     },
   },
