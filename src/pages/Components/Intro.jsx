bd6e9aaae2a464d100bff487b1b81a60c2b60d24
KGS97 <45433869+KGS97@users.noreply.github.com>
2023-01-25 23:37:09 +0530

    intro completed

--- src/pages/Components/Intro.jsx
+++ src/pages/Components/Intro.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 const Intro = () => {
-  let cls =
-    "bg-gradient-to-r from-cyan-500 to-teal-500 inline-block text-transparent bg-clip-text";
   return (
     <div>
-      <h1 className={"text-[60px] font-burtons font-thin " + cls}>
+      <h1
+        className={
+          "text-[60px] font-burtons font-thin bg-gradient-to-r from-cyan-500 to-teal-500 inline-block text-transparent bg-clip-text dark:bg-gradient-to-r dark:from-cyan-300 dark:to-teal-300"
+        }
+      >
         Hey There! I'm Dipanshu Singh.
       </h1>
       <br />
-      <p className="text-cyan-500 text-2xl font-medium font-poppins">
+      <p className="text-cyan-500 dark:text-cyan-200 text-2xl font-medium font-poppins">
         I'm a Front-End Web Developer.
       </p>
     </div>
