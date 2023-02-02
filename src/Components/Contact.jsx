f5db4128e3536f1a6761fd36146773ceb1877887
KGS97 <45433869+KGS97@users.noreply.github.com>
2023-02-03 02:05:49 +0530

    minor changes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,13 +5,13 @@ const Contact = () => {
   let close = "}";
   return (
     <div className=" text-left mx-8 text-xl lg:mx-48 font-poppins md:mx-24 text-cyan-700 dark:text-cyan-200 flex flex-col gap-8">
-      <p className="text-left">if(You want to work with me){open}</p>
+      <p className="text-left">if (You want to work with me) {open}</p>
       <p className="text-left">Lets get in touch.</p>
       <a href="https://www.linkedin.com/in/dipanshu-singh-645821153/">
         <AiFillLinkedin size={50} />
       </a>
       <p className="text-left">{close}</p>
-      <p className="text-left">else{open}</p>
+      <p className="text-left">else {open}</p>
       <p className="text-left">
         Connect on LinkedIn anyway. I appreciate you taking the time to know
         more about me.
